f59f9a8296c2f332f6310777bc3a8c710b1282bb
hyungjin <hyungzin0309@naver.com>
2023-12-02 03:16:47 +0900

    feat: flyweight

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item1/section3/Font.java
@@ -0,0 +1,13 @@
+package com.study.effectivejavastudy.chapters.chapter1.item1.section3;
+
+public class Font {
+
+    private int size;
+    private String fontType;
+
+    public Font(String fontType, int size) {
+        this.fontType = fontType;
+        this.size = size;
+    }
+
+}
