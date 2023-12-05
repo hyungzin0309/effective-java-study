25c93c3b1ebdf4fd02051bc913341b073e2b3b00
hyungjin <hyungzin0309@naver.com>
2023-12-05 22:52:46 +0900

    feat: singleton

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item2/section2/ComponentTest.java
@@ -0,0 +1,12 @@
+package com.study.effectivejavastudy.chapters.chapter1.item2.section2;
+
+import java.io.Serializable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@RequiredArgsConstructor
+public class ComponentTest implements Serializable {
+
+}
