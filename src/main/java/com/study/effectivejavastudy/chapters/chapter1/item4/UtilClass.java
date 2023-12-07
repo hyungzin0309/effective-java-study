c48ba011c2d8e9488c462c36a8d37bab3baaea86
hyungjin <hyungzin0309@naver.com>
2023-12-08 08:28:13 +0900

    feat: private constructor

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item4/UtilClass.java
@@ -0,0 +1,13 @@
+package com.study.effectivejavastudy.chapters.chapter1.item4;
+
+public class UtilClass {
+
+    private UtilClass() {
+        throw new AssertionError();
+    }
+
+    public static String hello() {
+        return "hello";
+    }
+
+}
