697c75b4bc95dff5158c73f2d742d2cbe1128540
hyungjin <hyungzin0309@naver.com>
2023-12-12 08:46:01 +0900

    feat: instance

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item6/Main.java
@@ -0,0 +1,13 @@
+package com.study.effectivejavastudy.chapters.chapter1.item6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String string = "hj,ks";
+        string.split(",");
+        string.matches(",");
+        string.replaceAll("old", "new");
+        string.replaceFirst("old", "new");
+    }
+
+}
