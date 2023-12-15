567a5da11497219dad7dc3fc42867e407633deaf
hyungjin <hyungzin0309@naver.com>
2023-12-15 14:34:48 +0900

    equals overriding

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item10/chapter2/RecordAddress.java
@@ -0,0 +1,9 @@
+package com.study.effectivejavastudy.chapters.chapter1.item10.chapter2;
+
+public record RecordAddress(
+        String country,
+        String city,
+        int numAddress
+) {
+
+}
