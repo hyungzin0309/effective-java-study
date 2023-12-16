6f588ecf56c841e1c563403e9ec2b929ba8b93c8
hyungjin <hyungzin0309@naver.com>
2023-12-16 17:49:34 +0900

    liskov principle

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item10/chapter3/Rectangle.java
@@ -0,0 +1,20 @@
+package com.study.effectivejavastudy.chapters.chapter1.item10.chapter3;
+
+public class Rectangle {
+
+    int height;
+    int width;
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getArea() {
+        return height * width;
+    }
+
+}
