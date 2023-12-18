747a6be35685f518fc48277ea0edc6f4beeea856
hyungjin <hyungzin0309@naver.com>
2023-12-19 08:31:34 +0900

    exception

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item13/Test.java
@@ -0,0 +1,15 @@
+package com.study.effectivejavastudy.chapters.chapter1.item13;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@Getter
+public class Test {
+
+    int value;
+
+}
+
