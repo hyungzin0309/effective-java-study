843009c77c76f59acc7e2da7167f9da5ef7f8a5a
hyungjin <hyungzin0309@naver.com>
2023-12-20 08:20:07 +0900

    compare

---
+++ src/main/java/com/study/effectivejavastudy/chapters/chapter1/item14/Test.java
@@ -0,0 +1,17 @@
+package com.study.effectivejavastudy.chapters.chapter1.item14;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Getter
+public class Test {
+
+    int value1;
+    long value2;
+    double value3;
+    String value4;
+
+}
